73072f554cbf53081e4c9ea99d18ca73ced76a83
Kaiden Bell <144740949+Kaiden-Bell@users.noreply.github.com>
2025-05-15 20:57:59 +0000

    Updated to add capturing for phase 2

---
+++ capture.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+
+SIGN_CLASS = 'A' # MUST BE CHANGED FOR EACH SIGN
+SAVE_DIR = f'data/fingerspellings{SIGN_CLASS}/'
+
+
+if not os.path.exists(SAVE_DIR):
+    os.makedirs(SAVE_DIR)
+
+cap = cv2.VideoCapture(0)
+count = 0
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1)
+
+    if key == ord('s'):
+        img_path = os.path.join(SAVE_DIR, f'{SIGN_CLASS}_{count}.jpg')
+        cv2.imwrite(img_path, frame)
+        print(f"[INFO] Saved: {img_path}")
+        count += 1
+
+    elif key == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
